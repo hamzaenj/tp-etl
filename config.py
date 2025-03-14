576182e117c732c1778382d7aa6b05d9ebb3a553
X2032984 <hamza.ennaji@ratp.fr>
2025-03-14 11:24:07 +0100

    fichier config

---
+++ config.py
@@ -0,0 +1,10 @@
+API_KEY = "56024ce4b5306416fa6fbb0b"
+API_URL = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/USD"
+
+# PostgreSQL - Informations de connexion
+DB_HOST = "localhost"
+DB_PORT = "5432"
+DB_USER = "postgres"
+DB_PASSWORD = "temp123"
+DB_NAME = "db_client"
+#56024ce4b5306416fa6fbb0b
